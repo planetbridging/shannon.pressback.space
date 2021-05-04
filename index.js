e3281f126775f34cad1248be03d5de84111093c7
planetbridging <47130739+planetbridging@users.noreply.github.com>
2021-05-05 03:08:10 +1000

    adding links

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import $ from "jquery";
 import styles from "./master.css";
-import home_page from './home.html';
+import home_page from './pages/home.html';
+import me_pic from './imgs/me.jpg';
 
 $(document).ready(function () {
     $( "#pills-home" ).html( home_page );
+    $("#PICme").html('<img id="Me" class="card-img-top" src="'+me_pic+'" alt="Card image cap">');
 });
   
