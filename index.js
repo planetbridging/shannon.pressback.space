c5de503831ef945e8018dbfcde235b06bc740f1c
planetbridging <47130739+planetbridging@users.noreply.github.com>
2021-05-05 11:00:23 +1000

    updated

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import $ from "jquery";
 import styles from "./master.css";
 import home_page from './pages/home.html';
+import work_study_page from './pages/worknstudy.html';
+import cert_of_comp from './pages/certofcomp.html';
 import me_pic from './imgs/me.jpg';
 
 $(document).ready(function () {
     $( "#pills-home" ).html( home_page );
+    $( "#pills-profile" ).html( work_study_page );
+    $( "#pills-contact" ).html( cert_of_comp );
     $("#PICme").html('<img id="Me" class="card-img-top" src="'+me_pic+'" alt="Card image cap">');
 });
   
